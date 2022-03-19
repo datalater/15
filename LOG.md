8a2d3c631dc018082e8d0de2bea474b202326de5
datalater <the7mincheol@gmail.com>
2022-03-19 23:06:10 +0900

    자바스크립트는 왜 프로토타입을 선택했을까

---
+++ LOG.md
@@ -0,0 +1,6 @@
+# 📜 LOG
+
+| Last modified | Article | Summary |
+| --- | --- | --- |
+| 2022-03-19 | [자바스크립트는 왜 프로토타입을 선택했을까](https://medium.com/@limsungmook/%EC%9E%90%EB%B0%94%EC%8A%A4%ED%81%AC%EB%A6%BD%ED%8A%B8%EB%8A%94-%EC%99%9C-%ED%94%84%EB%A1%9C%ED%86%A0%ED%83%80%EC%9E%85%EC%9D%84-%EC%84%A0%ED%83%9D%ED%96%88%EC%9D%84%EA%B9%8C-997f985adb42) | [javascript/why-js-prototype.md](./javascript/why-js-prototype.md) |
+| 2022-03-19 | [TypeScript는 어떻게 공부해야 하나요?](https://yozm.wishket.com/magazine/detail/1376/) | [typescript/how-to-learn-typescript.md](./typescript/how-to-learn-typescript.md) |
---
+++ script.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+# Added files in git
+files=$(git diff --cached --name-only --diff-filter=ACM)
+
+echo "Added files: $files"
+
+# Create LOG.md if it doesn't exist
+if ! [[ -f "LOG.md" ]]; then
+cat > LOG.md <<- EOF
+# 📜 LOG
+
+| Last modified | Article | Summary |
+| --- | --- | --- |
+EOF
+fi
+
+# Make table rows as looping through files
+rows=""
+newline=$'\n'
+for filename in $files
+do
+  # Skip if file is not markdown
+  if ! [[ $filename == *.md ]]; then
+    continue
+  fi
+
+  if [[ $filename == LOG.md ]]; then
+    continue
+  fi
+
+  headline=$(git blame -L 1,1 $filename)
+
+  if [[ -ne $headline ]]; then
+    last_modified=$(git blame $filename | grep -Eo '\b[[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2}\b' | sort -n | tail -1)
+    article=$(echo $headline | awk '{split($0, array, "# "); print array[2]}')
+
+    row="| $last_modified | $article | [$filename](./$filename) |"
+    rows+=${row}${newline}
+  fi
+done
+
+# Remove last new line for appending new rows next time script is run
+rows=${rows%${newline}}
+echo "Rows: $rows"
+
+# Append new rows to LOG.md
+if [[ -ne $rows ]]; then
+cat >> LOG.md <<- EOF
+$rows
+EOF
+
+git add LOG.md
+fi
+
+# Remove file if it doesn't contain any content
+lc=$(wc -l LOG.md | awk '{print $1}')
+defaultLines=4
+if ! [[ $lc -gt $defaultLines ]]; then
+  rm LOG.md
+fi
